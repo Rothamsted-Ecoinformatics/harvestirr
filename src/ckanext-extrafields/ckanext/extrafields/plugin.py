5c920f3187e71e6b7cd9689d2e43a611d86afdf1
root <root@ed-ubuntu-20-04.rothamsted.ac.uk>
2025-01-27 14:17:54 +0000

    extrafields

---
+++ src/ckanext-extrafields/ckanext/extrafields/plugin.py
@@ -0,0 +1,109 @@
+import ckan.plugins as plugins
+import ckan.plugins.toolkit as toolkit
+
+
+# import ckanext.extrafields.cli as cli
+# import ckanext.extrafields.helpers as helpers
+# import ckanext.extrafields.views as views
+# from ckanext.extrafields.logic import (
+#     action, auth, validators
+# )
+
+
+class ExtrafieldsPlugin(plugins.SingletonPlugin, toolkit.DefaultDatasetForm):
+    plugins.implements(plugins.IConfigurer)
+    plugins.implements(plugins.IDatasetForm) 
+    # plugins.implements(plugins.IAuthFunctions)
+    # plugins.implements(plugins.IActions)
+    # plugins.implements(plugins.IBlueprint)
+    # plugins.implements(plugins.IClick)
+    # plugins.implements(plugins.ITemplateHelpers)
+    # plugins.implements(plugins.IValidators)
+    
+
+    # IConfigurer
+
+    def update_config(self, config_):
+        toolkit.add_template_directory(config_, "templates")
+        toolkit.add_public_directory(config_, "public")
+        toolkit.add_resource("assets", "extrafields")
+   
+    def create_package_schema(self) -> Schema:
+        # let's grab the default schema in our plugin
+        schema: Schema = super(
+            ExampleIDatasetFormPlugin, self).create_package_schema()
+        # our custom field
+        schema.update({
+            'custom_text': [tk.get_validator('ignore_missing'),
+                            tk.get_converter('convert_to_extras')]
+        })
+        return schema
+    def update_package_schema(self) -> Schema:
+        schema: Schema = super(
+            ExampleIDatasetFormPlugin, self).update_package_schema()
+        # our custom field
+        schema.update({
+            'custom_text': [tk.get_validator('ignore_missing'),
+                            tk.get_converter('convert_to_extras')]
+        })
+        return schema
+    def show_package_schema(self) -> Schema:
+        schema: Schema = super(
+            ExampleIDatasetFormPlugin, self).show_package_schema()
+        schema.update({
+            'custom_text': [tk.get_converter('convert_from_extras'),
+                            tk.get_validator('ignore_missing')]
+        })
+        return schema
+        schema: Schema = super(
+            ExampleIDatasetFormPlugin, self).show_package_schema()
+        schema.update({
+            'custom_text': [tk.get_converter('convert_from_extras'),
+                            tk.get_validator('ignore_missing')]
+        })
+        return schema
+
+    def is_fallback(self):
+        # Return True to register this plugin as the default handler for
+        # package types not handled by any other IDatasetForm plugin.
+        return True
+
+    def package_types(self) -> list[str]:
+        # This plugin doesn't handle any special package types, it just
+        # registers itself as the default (above).
+        return []
+    def update_config(self, config: CKANConfig):
+        # Add this plugin's templates dir to CKAN's extra_template_paths, so
+        # that CKAN will use this plugin's custom templates.
+        tk.add_template_directory(config, 'templates')
+
+   # IAuthFunctions
+
+    # def get_auth_functions(self):
+    #     return auth.get_auth_functions()
+
+    # IActions
+
+    # def get_actions(self):
+    #     return action.get_actions()
+
+    # IBlueprint
+
+    # def get_blueprint(self):
+    #     return views.get_blueprints()
+
+    # IClick
+
+    # def get_commands(self):
+    #     return cli.get_commands()
+
+    # ITemplateHelpers
+
+    # def get_helpers(self):
+    #     return helpers.get_helpers()
+
+    # IValidators
+
+    # def get_validators(self):
+    #     return validators.get_validators()
+    
---
+++ src/ckanext-extrafields/setup.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+setup(
+    # If you are changing from the default layout of your extension, you may
+    # have to change the message extractors, you can read more about babel
+    # message extraction at
+    # http://babel.pocoo.org/docs/messages/#extraction-method-mapping-and-configuration
+    entry_points='''
+        [ckan.plugins]
+            ckanext-extrafields=ckanext.extrafields.plugin:ExampleIDatasetFormPlugin
+            ''',
+    message_extractors={
+        'ckanext': [
+            ('**.py', 'python', None),
+            ('**.js', 'javascript', None),
+            ('**/templates/**.html', 'ckan', None),
+        ],
+    }
+)
