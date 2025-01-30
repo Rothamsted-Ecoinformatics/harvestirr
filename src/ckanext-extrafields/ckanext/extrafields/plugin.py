5eb457418db1944e273f9d293809ae294284c1a4
Gokul Vijayakumar <gokul.vijayakumar-ram@rothamsted.ac.uk>
2025-01-30 17:02:01 +0000

    comment

--- src/ckanext-extrafields/ckanext/extrafields/plugin.py
+++ src/ckanext-extrafields/ckanext/extrafields/plugin.py
@@ -77,33 +77,5 @@ class ExampleIDatasetFormPlugin(plugins.SingletonPlugin, tk.DefaultDatasetForm):
         # that CKAN will use this plugin's custom templates.
         tk.add_template_directory(config, 'templates')
 
-   # IAuthFunctions
 
-    # def get_auth_functions(self):
-    #     return auth.get_auth_functions()
-
-    # IActions
-
-    # def get_actions(self):
-    #     return action.get_actions()
-
-    # IBlueprint
-
-    # def get_blueprint(self):
-    #     return views.get_blueprints()
-
-    # IClick
-
-    # def get_commands(self):
-    #     return cli.get_commands()
-
-    # ITemplateHelpers
-
-    # def get_helpers(self):
-    #     return helpers.get_helpers()
-
-    # IValidators
-
-    # def get_validators(self):
-    #     return validators.get_validators()
     
