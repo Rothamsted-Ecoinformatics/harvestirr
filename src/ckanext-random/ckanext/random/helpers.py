b00f85fbbef823a9b1ef294c89d8c5704d7dfe95
root <root@ed-ubuntu-20-04.rothamsted.ac.uk>
2025-06-04 13:46:01 +0100

    remove

--- src/ckanext-random/ckanext/random/helpers.py
+++
@@ -1,9 +0,0 @@
-
-def random_hello():
-    return "Hello, random!"
-
-
-def get_helpers():
-    return {
-        "random_hello": random_hello,
-    }
--- src/ckanext-random/ckanext/random/tests/test_helpers.py
+++
@@ -1,7 +0,0 @@
-"""Tests for helpers.py."""
-
-import ckanext.random.helpers as helpers
-
-
-def test_random_hello():
-    assert helpers.random_hello() == "Hello, random!"
