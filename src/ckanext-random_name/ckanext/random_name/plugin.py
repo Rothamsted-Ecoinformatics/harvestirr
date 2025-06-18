986b840d9eb682c3a3a48ff5b3f4a9cd771fe90d
GokulVijaakumarRam <gokul.vijayakumar-ram@rothamsted.ac.uk>
2025-06-18 16:43:09 +0100

    plugin

--- src/ckanext-random_name/ckanext/random_name/plugin.py
+++ src/ckanext-random_name/ckanext/random_name/plugin.py
@@ -3,39 +3,42 @@ import ckan.plugins as plugins
 import ckan.plugins.toolkit as toolkit
 from ckan.model import Package
 
+
 class RandomDatasetNamePlugin(plugins.SingletonPlugin):
-    plugins.implements(plugins.IConfigurer)
-    plugins.implements(plugins.IResourceUrlChange)
-    plugins.implements(plugins.ITemplateHelpers)
     plugins.implements(plugins.IConfigurer)
     plugins.implements(plugins.IDatasetForm)
 
     def update_config(self, config):
-        # Add our template and static files
         toolkit.add_template_directory(config, 'templates')
         toolkit.add_public_directory(config, 'public')
         toolkit.add_resource('fanstatic', 'random_name')
 
-    def get_helpers(self):
-        return {
-            'generate_random_dataset_name': self.generate_random_dataset_name
-        }
+    def create_package_schema(self):
+        schema = toolkit.default_create_package_schema()
+        if 'name' in schema:
+            del schema['name']  # remove to allow auto-generation
+        return schema
 
-    def generate_random_dataset_name(self):
-        """Helper to be used in template to generate a unique random name."""
+    def update_package_schema(self):
+        return toolkit.default_update_package_schema()
+
+    def show_package_schema(self):
+        return toolkit.default_show_package_schema()
+
+    def before_create(self, context, data_dict):
+        # Only generate name if it doesn't already exist
+        if not data_dict.get('name'):
+            data_dict['name'] = self._generate_unique_random_name()
+        return data_dict
+
+    def before_update(self, context, data_dict):
+        # Prevent name from being changed during update
+        if 'name' in data_dict:
+            del data_dict['name']
+        return data_dict
+
+    def _generate_unique_random_name(self):
         while True:
             candidate = str(random.randint(10000, 99999))
             if not Package.get(candidate):
                 return candidate
-
-    def before_create(self, context, data_dict):
-        # Set name only if not provided (i.e., during form load)
-        if not data_dict.get('name'):
-            data_dict['name'] = self.generate_random_dataset_name()
-        return data_dict
-
-    def before_update(self, context, data_dict):
-        # Do not change the name if already set
-        if 'name' in data_dict:
-            del data_dict['name']
-        return data_dict
