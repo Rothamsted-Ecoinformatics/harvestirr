3a91f495703c09b3ad151dd44e1cf03849fba7e9
GokulVijaakumarRam <gokul.vijayakumar-ram@rothamsted.ac.uk>
2025-06-18 16:57:34 +0100

    plugin

--- src/ckanext-random_name/ckanext/random_name/plugin.py
+++ src/ckanext-random_name/ckanext/random_name/plugin.py
@@ -1,30 +1,26 @@
 import random
-import ckan.plugins as plugins
-import ckan.plugins.toolkit as toolkit
-from ckan.model import Package
+import string
+from ckan.plugins import implements, SingletonPlugin, IDatasetForm
+from ckan.plugins import toolkit
 
-class RandomDatasetNamePlugin(plugins.SingletonPlugin):
-    plugins.implements(plugins.IConfigurer)
-   # plugins.implements(plugins.IPlugin)  # basic plugin interface
+class RandomDatasetNamePlugin(SingletonPlugin):
+    implements(IDatasetForm, inherit=True)
 
-    def update_config(self, config):
-        toolkit.add_template_directory(config, 'templates')
-        toolkit.add_public_directory(config, 'public')
-        toolkit.add_resource('fanstatic', 'random_name')
+    def _generate_random_name(self):
+        return 'dataset-' + ''.join(random.choices(string.ascii_lowercase + string.digits, k=8))
 
-    def before_create(self, context, data_dict):
-        if not data_dict.get('name'):
-            data_dict['name'] = self._generate_unique_random_name()
-        return data_dict
+    def create_package_schema(self):
+        schema = toolkit.default_create_package_schema()
 
-    def before_update(self, context, data_dict):
-        # Prevent changing name on edit
-        if 'name' in data_dict:
-            del data_dict['name']
-        return data_dict
+        def random_name(key, data, errors, context):
+            if not data.get('name'):
+                data['name'] = self._generate_random_name()
 
-    def _generate_unique_random_name(self):
-        while True:
-            candidate = str(random.randint(10000, 99999))
-            if not Package.get(candidate):
-                return candidate
+        schema['name'] = [random_name] + schema['name']
+        return schema
+
+    def update_package_schema(self):
+        return toolkit.default_update_package_schema()
+
+    def is_fallback(self):
+        return True  # Optional, only if needed
